a98761f0c7a69c6def213c1eb1d7f9a13c34f371
urnikita9 <urnikita9@gmail.com>
2025-07-04 22:25:37 +0300

    Remake input method for up to 8 binary digits

--- src/main/java/com/example/Bin2Dec.java
+++ src/main/java/com/example/Bin2Dec.java
@@ -6,7 +6,6 @@ public class Bin2Dec {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter up to 8 binary numbers by a space (press Enter to finish early):");
-            String result = "";
             int count = 0;
             while (count < 8) {
                 System.out.print("Enter binary number " + (count + 1) + ": ");
@@ -14,26 +13,7 @@ public class Bin2Dec {
                 if (input.isEmpty()) {
                     break;
                 }
-
-                boolean isBinary = true;
-                for (int i = 0; i < input.length(); i++) {
-                    char c = input.charAt(i);
-                    if (c != '0' && c != '1') {
-                        isBinary = false;
-                        break;
-                    }
-                }
-                if (!isBinary) {
-                    System.out.println("Error: Input must contain only 0s and 1s");
-                    continue;
-                }
-                if (!result.isEmpty()) {
-                    result += " ";
-                }
-                result += input;
-                count++;
             }
-            // System.out.println("Result: " + result);
         }
     }
 }
